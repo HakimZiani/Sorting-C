4f805256cdcbfed32764db5aa516baf79846c415
HakimZiani <51302362+HakimZiani@users.noreply.github.com>
2019-11-30 22:15:14 +0100

    Add files via upload

---
+++ main.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+void sort_vector(int algo,int order,int *vector)
+{
+  if algo==1
+}
+int main()
+{
+  int type=0,algo=0,order=0;
+  printf("Dear user, Please select the type of your structure : \n");
+  printf("1- Vector");
+  printf("2- Matrix");
+  printf("3- List");
+  scanf("%d",&type);
+
+  printf("Please choose the sorting algorithm you want to use :\n" );
+  printf("1- Bubble sort \n");
+  printf("2- Selecetion sort \n");
+  printf("3- Insertion sort \n");
+  scanf("%d",&algo);
+
+  printf("Please select the order of the sorting :\n" );
+  printf("1- Ascendent \n");
+  printf("2- Descedent \n");
+  scanf("%d",&order);
+
+  if (type==1)
+  {
+    /*Fonction vector with algo  and order */
+  }
+  else if(type==2)
+  {
+    /*Fonction 2 Matrix with algo and order  */
+  }
+  else if (type== 3)
+  {
+    /*fonction 3 witg algo and order */
+  }
+
+  return 0;
+}
